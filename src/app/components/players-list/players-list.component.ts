1e133037bbbfb623e19a11eac9073f25e513f62d
Rodrigo Soria <rodsilver83@hotmail.com>
2020-07-09 18:54:34 -0400

    feat(modal): add mini click open modal

--- src/app/components/players-list/players-list.component.ts
+++ src/app/components/players-list/players-list.component.ts
@@ -1,6 +1,7 @@
+import { Player } from './../../classes/player';
 import { PlayerDataService } from './../../services/player-data.service';
 import { GamePlayersService } from './../../services/game-players.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
 	selector: 'mc-players-list',
@@ -8,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./players-list.component.scss'],
 })
 export class PlayersListComponent implements OnInit {
+	public playerModal = null;
+	public modalActive = false;
+
 	constructor(
 		public gamePlayersService: GamePlayersService,
-		public playerDataService: PlayerDataService
+		public playerDataService: PlayerDataService,
+		private cd: ChangeDetectorRef
 	) {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.gamePlayersService.move$.subscribe((player: Player) => {
+			if (this.playerModal?.name === player?.name) {
+				this.playerModal = player;
+				this.cd.detectChanges();
+			}
+		});
+	}
+
+	openModal(player: Player) {
+		this.playerModal = player;
+		this.modalActive = true;
+		this.cd.detectChanges();
+	}
+
+	closeModal() {
+		this.modalActive = false;
+	}
 }
